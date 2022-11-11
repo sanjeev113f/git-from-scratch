18bbad9473c266fb72d5490f3a5089671a3be2bb
sanjeev113f <sanjeevkumar113f@gmail.com>
2022-11-11 21:38:36 +0530

    init added

---
+++ Building-git/mgit.kt
@@ -0,0 +1,18 @@
+import java.io.File
+import java.io.InputStream
+
+
+fun main(args: Array<String>) {
+
+    when (args[0]) {
+        "init" -> {
+            listOf(".git", ".git/objects", ".git/refs").forEach { File(it).mkdir() }
+            File(".git/HEAD").writeText("ref: refs/heads/master\n")
+            println("Initialized git directory")
+        }
+        
+
+       else -> throw IllegalArgumentException("Unknown command: ${args[0]}")
+    }
+
+}
